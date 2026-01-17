13feff22419634f7c42ce86644020f89dc85022b
astachowiak3 <astachowiak3@edu.cdv.pl>
2026-01-17 20:08:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 const animatedElements = document.querySelectorAll('.animate');
 
-
 const observer = new IntersectionObserver(entries => {
-entries.forEach(entry => {
-if (entry.isIntersecting) {
-entry.target.classList.add('show');
-}
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
 });
-});
-
 
 animatedElements.forEach(el => observer.observe(el));
